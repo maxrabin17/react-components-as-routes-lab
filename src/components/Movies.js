cd34e0c9f0b3ea1003bf6d61efb49824ca3a6f7c
maxrabin <max.rabin17@gmail.com>
2021-06-04 11:33:20 -0400

    commit tree 679542f845dafe67aadd3b876ccb69a555f4c0a0

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+    
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h1>{movie.title}</h1>
+          <h2>Movie Run Time: {movie.time} minutes</h2>
+          <p>Genres:</p>
+          <ul>
+            {movie.genres.map((genre, index) => (
+              <li key={index}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
